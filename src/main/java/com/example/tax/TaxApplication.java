7d95b4eb455a0735bd208d73f01c11293205a960
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2025-03-23 22:54:42 +0530

    Minor change

--- src/main/java/com/example/tax/TaxApplication.java
+++ src/main/java/com/example/tax/TaxApplication.java
@@ -2,46 +2,68 @@ package com.example.tax;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.util.Scanner;
 
 @SpringBootApplication
 public class TaxApplication {
 
 	public static void main(String[] args) {
-		// Take ClassPathXmlApplicationContext from applicationContext.xml file
-		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Tax propertyTax = (Tax) classPathXmlApplicationContext.getBean("propertyTax");
-		Tax incomeTax = (Tax) classPathXmlApplicationContext.getBean("incomeTax");
 
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the type of tax you want to pay: ");
-		// Input the type of tax from the user...
-		String type = scanner.nextLine();
-
-		System.out.println("Enter the taxable amount: ");
-		double amount = scanner.nextDouble();
-
-		if(type.equals("property")) {
-			System.out.println("Is property tax paid or not - " + propertyTax.isTaxPayed());
-			propertyTax.setTaxableAmount(amount);
-			propertyTax.calculateTaxAmount();
-			System.out.println("The tax amount to be paid is (in rupees): " + propertyTax.getTaxAmount());
-			propertyTax.payTax();
-			System.out.println("Is property tax paid or not - " + propertyTax.isTaxPayed());
+		System.out.println("Welcome to the Tax Payment Application");
+		while (true) {
+			System.out.println("Please select which tax you want to pay: \n1. Income \n2. Property\n3. Exit \n Choose a number...");
+			int userChoice = scanner.nextInt();
+			String taxChoice;
+			switch (userChoice) {
+				case 1 -> taxChoice = "incomeTax";
+				case 2 -> taxChoice = "propertyTax";
+				case 3 -> {
+					System.out.println("Exiting...");
+					return;
+				}
+				default -> {
+					System.out.println("Invalid choice");
+					return;
+				}
+			}
+			Tax tax = (Tax) context.getBean(taxChoice);
+			/*
+			1. Check if the tax is already paid, if yes print the message:
+			    "You have already paid Income/Property(get this getTaxType() interface method) tax."
+			2. If the tax is not paid:
+				a. Take the input of taxableAmount from the user.
+				b. Set the taxableAmount using setTaxableAmount() interface method.
+				c. Calculate the taxAmount using calculateTaxAmount() interface method.
+				d. Ask user if he wants to pay the tax. if yes call the payTax() method.
+			 */
+			if (tax.isTaxPayed()) {
+				if(taxChoice.equals("incomeTax")) {
+					System.out.println("You have already paid Income tax");
+				}
+				else {
+					System.out.println("You have already paid Property tax");
+				}
+			}
+			else {
+				System.out.println("Please enter your Income/Property value:");
+				// Taking the amount input from the user...
+				double input = scanner.nextDouble();
+				tax.setTaxableAmount(input);
+				tax.calculateTaxAmount();
+				System.out.println("You have selected " + tax.getTaxType() + " tax and your tax amount is: " + tax.getTaxAmount());
+				System.out.println("Do you want to pay the tax: \n1.Yes \n2.Exit \n Choose a number... ");
+				int val = scanner.nextInt();
+				switch (val) {
+					case 1:
+						tax.payTax();
+						break;
+					case 2:
+						break;
+				}
+			}
 		}
-
-		else if(type.equals("income")) {
-			System.out.println("Is income tax paid or not - " + incomeTax.isTaxPayed());
-			incomeTax.setTaxableAmount(amount);
-			incomeTax.calculateTaxAmount();
-			System.out.print("The tax amount to be paid is (in rupees): " + incomeTax.getTaxAmount());
-			incomeTax.payTax();
-			System.out.println("Is income tax paid or not - " + incomeTax.isTaxPayed());
-		}
-		else {
-			System.out.println("Please enter the correct type of tax!");
-		}
-
 	}
-
 }
